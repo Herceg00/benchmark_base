416d8766e4c632932f68b11c343e839b976baee6
Elijah <afanasiev_ilya@icloud.com>
2021-02-18 09:59:12 +0300

    - triada "METRIX" and iterations fixes

--- benchmarks_new/triada/triada_main.h
+++ benchmarks_new/triada/triada_main.h
@@ -19,36 +19,32 @@ typedef size_t helper_type[LENGTH];
 
 double CallKernel(int core_type)
 {
-    int triad_step_size[16] = {2,3,3,4,3,4,4,5,3,4,4,5,2,2,3,4};
 	static array_type a;
 	static array_type b;
 	static array_type c;
 	static array_type x;
 	static helper_type ind;
 
-
 	double time = -1;
 #ifndef METRIC_RUN
+    int triad_step_size[16] = {2,3,3,4,3,4,4,5,3,4,4,5,2,2,3,4};
     auto counter = PerformanceCounter();
     double flops_requested = LENGTH * 2;
     double bytes_requested = LENGTH * (triad_step_size[(int)MODE] * sizeof(double));
 #endif
-    int iterations;
-#ifndef METRIC_RUN
-    iterations = LOC_REPEAT;
-#endif
-#ifdef METRIX_RUN
-    iterations = LOC_REPEAT * 20;
+
+#ifdef METRIC_RUN
+    int iterations = LOC_REPEAT * 20;
     Init<base_type, array_type, helper_type>(core_type, a, b, c, x, ind, LENGTH);
+#else
+    int iterations = LOC_REPEAT;
 #endif
 
 	for(int i = 0; i < iterations; i++)
 	{
 #ifndef METRIC_RUN
 		Init<base_type, array_type, helper_type>(core_type, a, b, c, x, ind, LENGTH);
-
 		locality::utils::CacheAnnil(core_type);
-
         counter.start_timing();
 #endif
 
@@ -56,13 +52,11 @@ double CallKernel(int core_type)
 
 #ifndef METRIC_RUN
         counter.end_timing();
-
         counter.update_counters(bytes_requested, flops_requested);
-
         counter.print_local_counters();
 #endif
-
 	}
+
 #ifndef METRIC_RUN
     counter.print_average_counters(true);
     std::cout << "Benchmark type: " << (double) flops_requested / (double) bytes_requested<< " flops/byte";
