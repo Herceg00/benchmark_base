f4434b2b1c10ab1c0a0835ccb4f0e722f1c4e12f
Elijah <afanasiev_ilya@icloud.com>
2021-03-13 17:03:30 +0300

    - 1D stencil fix

--- benchmarks_new/run_tests.py
+++ benchmarks_new/run_tests.py
@@ -11,9 +11,8 @@ linear_length = 800000000
 
 all_tests_data = {"triada": {"mode": {"min": 0, "max": 9, "step": 1},
                               "length": linear_length},
-                  "stencil_1D": {"mode": {"min": 0, "max": 4, "step": 1},
-                                  "length": linear_length,
-                                  "radius": {"min": 1, "max": 12, "step": 1}},
+                  "stencil_1D": {"length": linear_length,
+                                 "radius": {"min": 1, "max": 12, "step": 1}},
                   "stencil_2D": {"mode": {"min": 0, "max": 1, "step": 1},
                                   "length": {"min": 256, "max": 32768, "step": "2_pow"},
                                   "radius": {"min": 1, "max": 3, "step": 1}},
